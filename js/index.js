9cebd6f6e761a9ac324dc210320b3ff18f772c66
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-18 21:54:46 +0100

    WIP: created click  event listener

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-let heroPhoto = document.querySelector('header img')
+const heroPhoto = document.querySelector('header img')
 
 heroPhoto.addEventListener('mouseover', () => {
     heroPhoto.setAttribute('src', "https://picsum.photos/id/11/1000/300")
@@ -9,8 +9,8 @@ heroPhoto.addEventListener('mouseout', () => {
     heroPhoto.setAttribute('src', "img/fun-bus.jpg")
 })
 
-let letsGoImage = document.querySelectorAll('.img-content img')[0]
-let adventureAwaitsImage = document.querySelectorAll('.img-content img')[1]
+const letsGoImage = document.querySelectorAll('.img-content img')[0]
+const adventureAwaitsImage = document.querySelectorAll('.img-content img')[1]
 
 document.addEventListener('keydown', () => {
     letsGoImage.setAttribute('src', 'https://picsum.photos/id/1/400/300')
@@ -22,10 +22,16 @@ document.addEventListener('keyup', () => {
     adventureAwaitsImage.setAttribute('src', 'img/fun.jpg')
 })
 
-let pNodeList = document.querySelectorAll('p')
+const pNodeList = document.querySelectorAll('p')
 pNodeList.forEach(function(pElement) {
     pElement.addEventListener('copy', () => {
         window.alert("No copying!");
     })
+})
+
+const mainLogo = document.querySelector('h1.logo-heading')
+// console.log(mainLogo)
+mainLogo.addEventListener('click', () => {
+    mainLogo.innerText = 'CLICKED'
 })
 
