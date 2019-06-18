623b29e856ae1cb3cda20e6f6a77835304b95da7
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-18 20:39:06 +0100

    WIP: created mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let heroPhoto = document.querySelector('header img')
+
+heroPhoto.addEventListener('mouseover', () => {
+    heroPhoto.setAttribute('src', "https://picsum.photos/id/11/1000/300")
+})
+
+heroPhoto.addEventListener('mouseout', () => {
+    heroPhoto.setAttribute('src', "img/fun-bus.jpg")
+})
+
+let letsGoImage = document.querySelectorAll('.img-content img')[0]
+let adventureAwaitsImage = document.querySelectorAll('.img-content img')[1]
+
+console.log(letsGoImage)
+console.log(adventureAwaitsImage)
